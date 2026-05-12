38dc8fa7b45e3dd8066fa3fa9e3bcab9401058ba
EDUARDO <vercosaeduardo@icloud.com>
2026-05-12 18:36:16 -0300

    Atualizar o cli.py

--- qobuz_dl/cli.py
+++ qobuz_dl/cli.py
@@ -469,7 +469,9 @@ def main():
                 target_dir = "\\\\?\\" + target_dir
                 
         try:
-            inject_lyrics_retroactively(target_dir, genius_token=genius_token)
+            # Captura a flag do terminal usando getattr(retorna False se a flag não for digitada)
+            overwrite_flag = getattr(arguments, 'overwrite', False)
+            inject_lyrics_retroactively(target_dir, genius_token=genius_token, overwrite=overwrite_flag)
         except KeyboardInterrupt:
             print("\n\n\033[91m[!] Operation manually interrupted by the user (CTRL+C).\033[0m")
             print("\033[93mAlready processed files are safe. Exiting...\033[0m")
