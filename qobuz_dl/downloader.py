29ca16ed5cd206ee5e543f3ce0bd7f3143e1022e
Carlos Eduardo Santos Vercosa <vercosaeduardo@icloud.com>
2026-05-19 19:56:58 -0300

    Atualizar o downloader.py

--- qobuz_dl/downloader.py
+++ qobuz_dl/downloader.py
@@ -672,7 +672,7 @@ class Download:
                         await safe_print_async(f"{OFF}   [*] Letra Encontrada: {track_no}. {track_title} - {search_artist} | Tradução: {trad_str} | Response_Code: {resp_code}{OFF}")
             else:
                 resp_str = resp_code if resp_code else "Não"
-                await safe_print_async(f"{RED}[ - ] Letra Não Encontrada: {track_no}. {track_title} - {search_artist} | Tradução: Não | Response_Code: {resp_str}{OFF}")
+                await safe_print_async(f"{RED}   [-] Letra Não Encontrada: {track_no}. {track_title} - {search_artist} | Tradução: Não | Response_Code: {resp_str}{OFF}")
 
         delay_time = getattr(self.settings, 'delay', 0)
         if delay_time == 0 and '--delay' in sys.argv:
